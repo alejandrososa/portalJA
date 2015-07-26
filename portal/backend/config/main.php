f8b7409919a00dc9f51d5db1a1c21cb76f9e8fa0
adoraxion <alesjohnson@hotmail.com>
2015-07-26 20:35:10 +0200

    Esqueleto Portal JA

---
+++ portal/backend/config/main.php
@@ -0,0 +1,47 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        //
+        'request'=>[
+            //'baseUrl'=>'/backend',
+            'class' => 'common\components\Request',
+            'web'=> '/backend/web',
+            'adminUrl' => '/admin'
+        ],
+        'urlManager'=>[
+            //'scriptUrl'=>'/backend/index.php',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
+        //
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
